9b50fb1232569baeaac1915e5032906b75f5244b
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2025-04-21 19:57:11 +0600

    testng/part 1

---
+++ src/test/java/config/Setup.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class Setup {
+   public WebDriver driver;
+
+    @BeforeTest
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://dailyfinance.roadtocareer.net/login");
+//        driver.get("https://dailyfinance.roadtocareer.net/");
+    }
+
+    //@AfterTest
+    public void tearDown(){
+//        page.LoginPage loginPage = new page.LoginPage(driver);
+//        loginPage.doLogout();
+
+//        driver.quit();
+
+    }
+}
