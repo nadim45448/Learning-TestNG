0441601e353b606caeb0e3ea0c455cf6e7cd69a4
nadim45448 <nadimhossain.nkh.1007@gmail.com>
2025-04-22 00:54:20 +0600

    testing part 2

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,5 +1,15 @@
 package utils;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Utils {
     public static int generateRandomNumber(int min, int max){
         double randomNumber= Math.random()*(max-min)+min;
@@ -10,4 +20,18 @@ public class Utils {
         int id= generateRandomNumber(1000,9999);
         System.out.println(id);
     }
+
+    public static void saveUserData(JSONObject obj, String filePath) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+       JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(filePath));
+       jsonArray.add(obj);
+        FileWriter writer = new FileWriter(filePath);
+        writer.write(jsonArray.toJSONString());
+        writer.flush();
+        writer.close();
+
+
+    }
+
+
 }
